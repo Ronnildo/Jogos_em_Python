f3edc15ae9d79eb32332b968f770da5df4d5353f
Ronnildo <ronildosp03@gmail.com>
2020-04-28 14:53:36 -0300

    Jogo da Forca V2

--- forca.py
+++ forca.py
@@ -9,19 +9,28 @@ letras_acertadas = ['_', '_', '_', '_', '_', '_']
 
 acertou = False
 enforcou = False
+erros = 0
+
+print(letras_acertadas)
 
 while not acertou and not enforcou:
     chute = input('Qual a letra: ')
 
     posicao = 0
-
-    for letra in palavra_secreta:
-        if chute.upper() == letra.upper():
-            letras_acertadas[posicao] = letra
-        posicao = posicao + 1
-
+    if chute in palavra_secreta:
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                letras_acertadas[posicao] = letra
+            posicao = posicao + 1
+    else:
+        erros += 1
+    
+    acertou = '_' not in letras_acertadas
+    enforcou =  erros == 6
     print(letras_acertadas)
 
-
-print('Jogando...')
+if acertou:
+    print('Você ganhou!!')
+else:
+    print('Você perdeu!!')
 print('Fim de Jogo')
